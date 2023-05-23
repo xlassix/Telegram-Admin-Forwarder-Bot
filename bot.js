339dd19662a425214ea9660990c87d732e76daf8
Precious Mozia <chibundumozia@gmail.com>
2023-05-22 19:54:51 -0700

    Telegram Admin Forwarder Bot

--- bot.js
+++ bot.js
@@ -1,15 +1,13 @@
 const TelegramBot = require('node-telegram-bot-api');
-// Replace 6149662006:AAEDS31IDdZAtuREqoVJu3W63i3nHnV7Uvo with 'YOUR_API_TOKEN'
-const bot = new TelegramBot('6149662006:AAEDS31IDdZAtuREqoVJu3W63i3nHnV7Uvo', { polling: true });
+const bot = new TelegramBot('YOUR_API_TOKEN', { polling: true });
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
   
     // Check if the message is sent by the admin
-    // Replace 1267334388 with 'ADMIN_ID'
-    if (msg.from.id === 1267334388) {
+    if (msg.from.id === ADMIN_ID) {
       // Forward the message to the specified group
       // Replace -758309838 with 'GROUP_ID' 
-      bot.forwardMessage(-758309838, chatId, msg.message_id);
+      bot.forwardMessage(GROUP_ID, chatId, msg.message_id);
     }
   });
   
