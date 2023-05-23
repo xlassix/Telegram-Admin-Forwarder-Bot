e518a3d86e555c7b7499470d21f109ccae54e59e
Precious Mozia <chibundumozia@gmail.com>
2023-05-23 15:06:55 -0700

    Telegram Admin Forwarder Bot

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot('YOUR_API_TOKEN', { polling: true });
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-  
-    // Check if the message is sent by the admin
-    if (msg.from.id === ADMIN_ID) {
-      // Forward the message to the specified group
-      bot.forwardMessage(GROUP_ID, chatId, msg.message_id);
-    }
-  });
-  
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+
+const bot = new TelegramBot(process.env.BOT_API_TOKEN, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  // Check if the message is sent by the admin
+  if (msg.from.id === process.env.ADMIN_ID) {
+    // Forward the message to the specified group
+    bot.forwardMessage(process.env.GROUP_ID, chatId, msg.message_id);
+  }
+});
